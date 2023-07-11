18831f851f731af3b5bf3a91a02bbacebbab527e
Vivek Sutar <viveksutar26@gmail.com>
2023-07-11 12:34:47 +0530

    first commit

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -7,15 +7,21 @@ import { SellerService } from './services/seller.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
   constructor(private sellerService: SellerService) { }
+
   canActivate(
+
     route: ActivatedRouteSnapshot,
+
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
     if (localStorage.getItem("seller")) {
+
       return true;
+      
     }
-    
+
     return this.sellerService.isSellerLoggedIn;
 
   }
